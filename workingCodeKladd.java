920bdf373efd2730e65069a421ef7718617296f6
Olavlel <olav.ellingsen@gmail.com>
2025-10-28 18:47:13 +0100

    rename kladdefil

--- workingCodeKladd.java
+++ workingCodeKladd.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 
-public class workingCodeKladd {
+public class WorkingCodeKladd {
     public static void main(String args[]) {
         File movies = new File("six-degrees-of-imdb-ressursside/marvel_actors.tsv");
         File actors = new File("six-degrees-of-imdb-ressursside/marvel_movies.tsv");
