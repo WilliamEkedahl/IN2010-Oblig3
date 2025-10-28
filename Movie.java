2780cff2e53e47e2ee42d0bfef77bc284d0057b3
WilliamEkedahl <altinus.ekedahl@gmail.com>
2025-10-28 18:14:05 +0100

    Movies uploaded

--- Movie.java
+++ Movie.java
@@ -1,2 +1,22 @@
+import java.io.*;
+import java.util.*;
+
 public class Movie {
+
+    String titel;
+    String rating;
+
+    public static void movie(String ttid, String titel, double rating){
+        this.ttid = ttid;
+        this.titel = titel;
+        this.rating = rating;
+    }
+
+   ArrayList<Movie> movies = new ArrayList<Movie>();
+
+    @Ovveride
+    public String toString(String ttid, String titel, double rating){
+        String movie = System.out.printf("this is the list of movies %s %s %s%n", ttid, titel, rating);
+        return movie;
+    }
 }
