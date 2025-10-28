9ee9cae17819a3aa51ac25eb51919970a1e959f1
Olavlel <olav.ellingsen@gmail.com>
2025-10-28 19:19:03 +0100

    Readfile to main, plus changes

--- Main.java
+++ Main.java
@@ -4,43 +4,40 @@ import java.util.Map;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 
-public class ReadFile {
+public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Graph<String> marvelGraph = new Graph<>();
-        File actors = new File("six-degrees-of-imdb-ressursside/marvel_actors.tsv");
+        //File actors = new File("six-degrees-of-imdb-ressursside/marvel_actors.tsv");
         
 
-        //Read movies
-        //readFile<>(movies);
 
-        //Read actors and generate the graph
-        //readFile(actors);
-
-        //print info about the graph
-        marvelGraph.countNodes();
-        marvelGraph.countEdges();
 
         ArrayList<Movie> movieList = new ArrayList<Movie>();
 
-        Map<String, Movie> movies = new Hashmap<>();
-        {
+        //Map<String, Movie> movies = new HashMap<>();
+        
             try (Scanner fileReader = new Scanner(new File("six-degrees-of-imdb-ressursside/marvel_actors.tsv"))) {
                 while (fileReader.hasNextLine()) {
                     //store the data in fileData, continue if theres another line and remove spaces at the end
-                    String[] parts = fileReader.nextLine().trim();
-                    //if line is empty skip it
-                    if (String.isEmpty()) continue;
+                    String line = fileReader.nextLine().trim();
                     String[] parts = line.split("\t");
-                    String ttid = line.parts[0];
-                    String titel = line.parts[1];
+                    
+                    String ttid = parts[0];
+                    String title = parts[1];
                     double rating = Double.parseDouble(parts[2]);
-                    movies.put(ttid, new Movie(title, rating));
+                    Movie temp = new Movie(ttid,title,rating);
+                    movieList.add(temp);
                 }
             } catch (FileNotFoundException e) {
                 System.out.println("The file could not be read while reading movies");
                 e.printStackTrace();
             }
-        }
+        
+
+
+                //print info about the graph
+        marvelGraph.countNodes();
+        marvelGraph.countEdges();
     }
 }
 
