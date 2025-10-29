4d5a6aece4b072c730b8638a4bef2cd8846d6e86
Olavlel <olav.ellingsen@gmail.com>
2025-10-29 14:33:10 +0100

    byttet navn

--- workingCodeKladd.java
+++ workingCodeKladd.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 public class WorkingCodeKladd {
+
+
+
+    
+
+
     public static void main(String args[]) {
         File movies = new File("six-degrees-of-imdb-ressursside/marvel_actors.tsv");
         File actors = new File("six-degrees-of-imdb-ressursside/marvel_movies.tsv");
